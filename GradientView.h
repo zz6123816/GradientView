a7789a88b67ed375b9b98960c03892180dae5099
康健 <412311971@qq.com>
2016-03-24 22:29:20 +0800

    渐变的UIView

---
+++ GradientView.h
@@ -0,0 +1,32 @@
+//
+//  GradientView.h
+//  GiftBox
+//
+//  Created by 康健 on 16/1/15.
+//  Copyright © 2016年 xinyihezi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+typedef NS_ENUM(NSInteger, GradientViewType){
+    /** 水平渐变 */
+    kGradientViewTypeHorizontal = 0,
+    /** 竖直渐变 */
+    kGradientViewTypeVertical = 1,
+    /** 左斜渐变 */
+    kGradientViewTypeInclinedLeft = 2,
+    /** 右斜渐变 */
+    kGradientViewTypeInclinedRight = 3
+};
+/** 渐变颜色的UIView */
+@interface GradientView : UIView
+/** 起始颜色 */
+@property (nonatomic, strong) UIColor *startColor;
+/** 结束颜色 */
+@property (nonatomic, strong) UIColor *endColor;
+/** 渐变类型 */
+@property (nonatomic, assign) GradientViewType gradientViewType;
+/** 起始的偏移量 默认是0 */
+@property (nonatomic, assign) CGFloat startOffset;
+- (id)initWithFrame:(CGRect)frame startColor:(UIColor *)startColor endColor:(UIColor *)endColor;
+- (void)removeGradient;
+@end
